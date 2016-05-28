d646d203bbf294d593e80241ac8ae3b7a6fcbe46
Armin <armin.bagheri.1380@gmail.com>
2016-05-28 16:50:43 +0430

    Create Flood.lua

---
+++ plugins/Flood.lua
@@ -0,0 +1,45 @@
+local function run(msg)
+
+if msg.text == "/id" or msg.text == "/rules" or msg.text == "/help" or msg.text == "/about" or msg.text == "/owner" or msg.text == "/modlist" then
+	return "/id"
+end
+
+if msg.text == "/id" or msg.text == "/rules" or msg.text == "/help" or msg.text == "/about" or msg.text == "/owner" or msg.text == "/modlist" then
+	return "/rules"
+end
+
+if msg.text == "/id" or msg.text == "/rules" or msg.text == "/help" or msg.text == "/about" or msg.text == "/owner" or msg.text == "/modlist" then
+	return "/help
+end
+
+if msg.text == "/id" or msg.text == "/rules" or msg.text == "/help" or msg.text == "/about" or msg.text == "/owner" or msg.text == "/modlist" then
+	return "/about"
+end
+
+if msg.text == "/id" or msg.text == "/rules" or msg.text == "/help" or msg.text == "/about" or msg.text == "/owner" or msg.text == "/modlist" then
+	return "/owner"
+end
+
+if msg.text == "/id" or msg.text == "/rules" or msg.text == "/help" or msg.text == "/about" or msg.text == "/owner" or msg.text == "/modlist" then
+	return "/modlist"
+end
+
+if msg.text == "/start then
+	return " Hi \n This BOT Is Spammer \n\n Developer: @TelePublic "
+end
+
+end
+
+return {
+patterns = {
+    "/id",
+    "/rules",
+    "/help",
+    "/about",
+    "/owner",
+    "/modlist",
+    "/start",
+    }, 
+run = run,
+pre_process = pre_process
+}
