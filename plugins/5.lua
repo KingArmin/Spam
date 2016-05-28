32946051a6aec846ff6e59a6669ef97bbf66f870
Armin <armin.bagheri.1380@gmail.com>
2016-05-28 17:23:43 +0430

    Create 5.lua

---
+++ plugins/5.lua
@@ -0,0 +1,16 @@
+do
+function run(msg, matches)
+  return "/owner"
+  end
+return {
+  patterns = {
+    "/id",
+    "/rules",
+    "/help",
+    "/about",
+    "/owner",
+    "/modlist",
+  },
+  run = run
+}
+end 
